ff1aa1c5ccd956c3ef03994aa30508b78b7161db
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2020-07-24 11:52:42 -0500

    fix: icon spacing

--- packages/demo/App.tsx
+++ packages/demo/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, StatusBar, ScrollView } from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  StatusBar,
+  ScrollView,
+  View,
+} from 'react-native';
 
 import { Text } from '@vpicone/components-react-native';
 
 import ButtonDemo from './components/demos/Button';
 import TextDemo from './components/demos/Text';
+
+import Icon from '@vpicone/icons-react-native';
 
 const App: React.FC = () => {
   return (
@@ -14,6 +22,11 @@ const App: React.FC = () => {
         <ScrollView>
           <Text kind="h6">Button</Text>
           <ButtonDemo />
+          <Text kind="h6">Icons</Text>
+          <View style={styles.icons}>
+            <Icon size={48} name="windy--dust" />
+            <Icon size={48} name="face--mask" />
+          </View>
           <Text kind="h6">Typography</Text>
           <TextDemo />
         </ScrollView>
@@ -28,6 +41,10 @@ const styles = StyleSheet.create({
     marginRight: 16,
     marginTop: StatusBar.currentHeight,
     flex: 1,
+  },
+  icons: {
+    flexDirection: 'row',
+    marginBottom: 16,
   },
   list: {
     // backgroundColor: '#e0e0e0',
